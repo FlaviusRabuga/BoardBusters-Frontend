6681e6b71ff82fc3d5b5ec9694bb3c6f61850db9
Flavius Rabuga <flavius.rabuga02@gmail.com>
2024-05-13 10:57:05 +0300

    login touch ups

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,53 +1,69 @@
+'use client';
 import React from "react";
 import "./style.css";
 import Link from 'next/link';
 
 
 export default function Page() {
-      
-  return (
-    <div className="containerLogIn">
-                <div className="pagina">
-                    <div className="headerLog"><div className="scris">Login</div></div>
-                    <div className="input1"> <input className="inputLog" type="text" placeholder="Username" name = "username" 
-                    // onChange={(e) => this.onChangeHandler(e)}
-                    >
-                      </input> </div>
-                    <div className="input2"> <input className="inputLog" type="password" placeholder="Password" password = "password" 
-                    // onChange={(e) => this.onChangeHandler(e)}
-                    >
-                      </input> </div>
-                  
-                    <div> <button className="buttonLog"  
 
-                    // onClick={
-                        // async () => {
-                        //     let error = await this.send_login_data();
-                        //     console.log(error);
-                            
-                        //     if(!error)
-                        //     {
-                        //         localStorage.setItem("username", this.state.username);
-                                
-                        //         this.setState({logOK: true});
-                        //     }
-                        // }
-                    
-                    // }
-                    >Login</button> </div>
-                    <div> 
-                    <Link href="/register">
-                        <button className="buttonLog1">Need an account?</button>
-                    </Link>
-                    </div>
-                    
-                </div>
-            </div>
+  function validateInput(username, password) {
+    if (username === '' || password === '') {
+      alert('Please fill in all fields');
+      return false;
+    }
+    return true;
+  }
+
+  async function sendLoginData(event) {
+
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const username = data.get('username');
+    const password = data.get('password');
+    console.log(username);
+    console.log(password);
+
+    const valid =  validateInput(username, password);
+
+    const response = await fetch('http://localhost:5000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+  }
+
+  return (
+    <form className="containerLogIn" onSubmit={sendLoginData}>
+      <div className="pagina">
+        <div className="headerLog">
+          <div className="scris">Login
+          </div>
+        </div>
+        <div className="input1"> <input className="inputLog" type="text" placeholder="Username" name="username">
+        </input> </div>
+        <div className="input2"> <input className="inputLog" type="password" placeholder="Password" password="password" name="password">
+        </input> </div>
+
+        <div> <button className="buttonLog"
+        >Login</button> </div>
+        <div>
+          <Link href="/register">
+            <button className="buttonLog1">Need an account?</button>
+          </Link>
+        </div>
+
+      </div>
+    </form>
   );
 
-  
 
 
 
 
-  }
+
+}
